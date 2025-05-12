96cc9e30bf4cfc234a5ba3a4fba969b2ed9d5e10
MiguelMartoni <miguelmartoni26@gmail.com>
2025-05-12 11:36:06 -0300

    Inicialização do Projeto

---
+++ src/main/java/com/FarmaPet/FarmaPet/FarmaPetApplication.java
@@ -0,0 +1,13 @@
+package com.FarmaPet.FarmaPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FarmaPet/FarmaPet/FarmaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FarmaPet.FarmaPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
