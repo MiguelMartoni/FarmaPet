40f54f1eb69f35ae58f246cc0acb1a8ec5fbca5a
MiguelMartoni <miguelmartoni26@gmail.com>
2025-06-03 20:49:04 -0300

    Ajuste na DTO de animal

--- src/main/java/com/FarmaPet/FarmaPet/dtos/DtoAnimal.java
+++ src/main/java/com/FarmaPet/FarmaPet/dtos/DtoAnimal.java
@@ -2,15 +2,14 @@ package com.FarmaPet.FarmaPet.DTOs;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record DtoAnimal(
-
-        Integer id,    
         @NotBlank String nome,
         @NotBlank String especie,
         @NotBlank String raca,
-        @Min(0)     Integer idade,
-        @Positive   Float   peso
-
+        @Min(0) Integer idade,
+        @Positive Float peso,
+        @NotNull Integer clienteId
 ) {}
